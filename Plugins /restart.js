dac64561629553d53ba688cf9a41b37fa2aa75f8
𒅒𝗔𝗧𝗢𝗠𝗜𝗖ᵗᵉᶜʰ✰ཽ <akuebenjamin01@gmail.com>
2025-01-26 20:34:19 +0000

    Update restart.js

--- Plugins /restart.js
+++ Plugins /restart.js
@@ -4,14 +4,14 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
-    desc: "restart the bot",
+    desc: "redémarrer le bot",
     category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const {exec} = require("child_process")
-reply("restarting...")
+reply("💁‍♂️𝙍𝙀𝘿𝙀𝙈𝘼𝙍𝙍𝘼𝙂𝙀卍■■■□□ 60%")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
